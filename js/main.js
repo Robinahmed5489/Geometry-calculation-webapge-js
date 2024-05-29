13ae599a1729a29a631b2e0b079abd487113e959
Robinahmed5489 <robsak5489@gmail.com>
2024-05-29 19:46:35 +0600

    final submission

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ function calculateTriangleArea() {
     return;
   }
   const triangleArea = 0.5 * triangleBaseValue * triangleHeightValue;
-  setData("triangle-update-area", triangleArea);
+  const triangleAreaThreeDecimal = triangleArea.toFixed(3);
+  setData("triangle-update-area", triangleAreaThreeDecimal);
 }
 // rectabgle
 
@@ -18,9 +19,10 @@ function calculateRectangleArea() {
     return;
   }
   const rectangleArea = RectangleWidthValue * RectangleHeightValue;
-  setData("rectangle-update-area", rectangleArea);
+  const RectangleAreaThreeDecimal = rectangleArea.toFixed(3);
+  setData("rectangle-update-area", RectangleAreaThreeDecimal);
 }
-
+// Parallelogram
 function calculateParallelogramArea() {
   const ParallelogramBaseValue = getInputById("Parallelogram-base");
   const ParallelogramHeightValue = getInputById("Parallelogram-height");
@@ -29,5 +31,53 @@ function calculateParallelogramArea() {
     return;
   }
   const ParallelogramArea = ParallelogramBaseValue * ParallelogramHeightValue;
-  setData("Parallelogram-update-area", ParallelogramArea);
+  const ParallelogramAreaThreeDecimal = ParallelogramArea.toFixed(3);
+  setData("Parallelogram-update-area", ParallelogramAreaThreeDecimal);
+}
+// Rhombus
+
+function calculateRhombusArea() {
+  const RhombusD1Value = getInputById("Rhombus-d1");
+  const RhombusD2Value = getInputById("Rhombus-d2");
+  if (isNaN(RhombusD1Value) || isNaN(RhombusD2Value)) {
+    alert("Please Enter Appropriate Measurements");
+    return;
+  }
+  const RhombusArea = 0.5 * RhombusD1Value * RhombusD2Value;
+  const RhombusAreaThreeDecimal = RhombusArea.toFixed(3);
+  setData("Rhombus-update-area", RhombusAreaThreeDecimal);
+}
+
+// Pentagon
+function calculatePentagonArea() {
+  const PentagonPValue = getInputById("Pentagon-p");
+  const PentagonBValue = getInputById("Pentagon-b");
+  if (isNaN(PentagonPValue) || isNaN(PentagonBValue)) {
+    alert("Please Enter Appropriate Measurements");
+    return;
+  }
+  const PentagonArea = 0.5 * PentagonPValue * PentagonBValue;
+  const PentagonAreaThreeDecimal = PentagonArea.toFixed(3);
+  setData("Pentagon-update-area", PentagonAreaThreeDecimal);
+  areaCalculationEntry("Pentagon", PentagonAreaThreeDecimal);
+}
+// Ellipse
+function calculateEllipseArea() {
+  const EllipseAValue = getInputById("Ellipse-a-radius");
+  const EllipseBValue = getInputById("Ellipse-b-radius");
+  if (isNaN(EllipseAValue) || isNaN(EllipseBValue)) {
+    alert("Please Enter Appropriate Measurements");
+    return;
+  }
+  const EllipseArea = 3.14 * EllipseAValue * EllipseBValue;
+  const EllipseAreaThreeDecimal = EllipseArea.toFixed(3);
+  setData("Ellipse-update-area", EllipseAreaThreeDecimal);
+  areaCalculationEntry("Ellipse", EllipseAreaThreeDecimal);
+}
+function Convert() {
+  const previousValue = getElementById("value");
+  const newValue = previousValue / (100 * 100);
+  const newValueTwoDecimal = newValue.toFixed(4);
+  setData("value", newValueTwoDecimal);
+  setEntryDataUnit("unit");
 }
--- js/utitlities.js
+++ js/utitlities.js
@@ -13,3 +13,22 @@ function setData(targetId, value) {
   const targetElementTag = document.getElementById(targetId);
   targetElementTag.innerText = value;
 }
+function setEntryDataUnit(targetId) {
+  const targetElementTag = document.getElementById("unit");
+  targetElementTag.innerText = "m";
+}
+
+function areaCalculationEntry(ShapeName, ShapeArea) {
+  const keepEntries = document.getElementById("entries");
+  const p = document.createElement("p");
+  p.classList.add("my-2");
+  const count = keepEntries.childElementCount;
+
+  p.innerHTML = `${
+    count + 1
+  }.${ShapeName}=  <span id="value">${ShapeArea}</span> <span id="unit">cm</span><sup>2</sup> 
+  <button onclick=Convert(this) 
+  class="text-sm bg-green-600 text-white py-1 px-2 rounded-sm">
+  Convert</button>`;
+  keepEntries.appendChild(p);
+}
