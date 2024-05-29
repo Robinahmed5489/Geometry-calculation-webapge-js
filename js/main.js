0a9a5d0e2f92384f1ca61755c08ed549d43577ff
Robinahmed5489 <robsak5489@gmail.com>
2024-05-29 18:15:10 +0600

    some commit

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ function calculateTriangleArea() {
   const triangleArea = 0.5 * triangleBaseValue * triangleHeightValue;
   setData("triangle-update-area", triangleArea);
 }
+// rectabgle
+
+function calculateRectangleArea() {
+  const RectangleWidthValue = getInputById("rectangle-width");
+  const RectangleHeightValue = getInputById("rectangle-length");
+  if (isNaN(RectangleWidthValue) || isNaN(RectangleHeightValue)) {
+    alert("Please Enter Appropriate Measurements");
+    return;
+  }
+  const rectangleArea = RectangleWidthValue * RectangleHeightValue;
+  setData("rectangle-update-area", rectangleArea);
+}
+
+function calculateParallelogramArea() {
+  const ParallelogramBaseValue = getInputById("Parallelogram-base");
+  const ParallelogramHeightValue = getInputById("Parallelogram-height");
+  if (isNaN(ParallelogramBaseValue) || isNaN(ParallelogramHeightValue)) {
+    alert("Please Enter Appropriate Measurements");
+    return;
+  }
+  const ParallelogramArea = ParallelogramBaseValue * ParallelogramHeightValue;
+  setData("Parallelogram-update-area", ParallelogramArea);
+}
